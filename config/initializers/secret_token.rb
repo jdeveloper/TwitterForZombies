4fcb3c7f83be3fb2ebaa5a13a1b5695f95295d08
j.fernandez <jdeveloper.inxenio@gmail.com>
2013-03-11 13:44:03 +0100

    Creado esqueleto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweeterForZombies::Application.config.secret_token = 'ab576ff10528c8a9523617d1e30e967a10f575bb1534f0748004cd672cee51ca6bfe49a1b55dbb1e801d4eae89a096138edff3bfb3f418817fdcf52b5c0a04f3'
