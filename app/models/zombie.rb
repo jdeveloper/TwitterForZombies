d7b5b768aa4d1f2c01903a770656a55b0ff599cc
j.fernandez <jdeveloper.inxenio@gmail.com>
2013-03-13 12:11:16 +0100

    Added a callback to mark the zombie as rotting if his age is greater than 20

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,7 +1,18 @@
 class Zombie < ActiveRecord::Base
   attr_accessible :age, :bio, :name
   
+  before_save :make_rotting
+  
   scope :rotting, where(rotting: true)
   scope :fresh, where("age < 20")
-  scope :recen, order("created_at desc").limit(3)
+  scope :recent, order("created_at desc").limit(3)
+  
+  def make_rotting
+    self.rotting = isOlderThen20?
+    true
+  end
+  
+  def isOlderThen20?
+    age > 20
+  end
 end
