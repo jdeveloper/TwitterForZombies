add81b77a545ed0edbec03a3c16b0b7071ce53a4
j.fernandez <jdeveloper.inxenio@gmail.com>
2013-03-13 12:19:14 +0100

    Added model brain

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313094210) do
+ActiveRecord::Schema.define(:version => 20130313111824) do
 
   create_table "zombies", :force => true do |t|
     t.string   "name"
     t.text     "bio"
-    t.integer  "age"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "email"
