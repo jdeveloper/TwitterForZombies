5e08956abc20f160497eb47c6c20930df54c897b
j.fernandez <jdeveloper.inxenio@gmail.com>
2013-03-26 11:56:06 +0100

    Added validation of presence of graveyard

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,7 +1,7 @@
 class Zombie < ActiveRecord::Base
   attr_accessible :age, :bio, :name, :email, :decomposition
   
-  validates_presence_of :name
+  validates_presence_of :name, :graveyard
   
   has_one :brain, dependent: :destroy
   has_many :tweets
